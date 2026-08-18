e8e48601b7053bd3a6d66108ebec0c8376aac06e
Analia Loiacono <analialoiacono@Analias-MacBook-Air.local>
2026-08-18 18:16:47 -0300

    Primera versión de interfaz web

--- app.py
+++ app.py
@@ -1,10 +1,24 @@
-from flask import Flask
+from flask import Flask, render_template
+import json
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def inicio():
-    return "¡Bienvenido a Training Point Manager!"
+    return render_template("index.html")
+
+
+@app.route("/alumnos")
+def mostrar_alumnos():
+    with open("alumnos.json", "r") as archivo:
+        alumnos = json.load(archivo)
+
+    return render_template("alumnos.html", alumnos=alumnos)
+
+@app.route("/agregar")
+def agregar_alumno():
+    return render_template("agregar.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
