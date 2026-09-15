e315ca09ad200cf0066ec2451e58b9fe1880b0a2
Analia Loiacono <analialoiacono@Analias-MacBook-Air.local>
2026-09-15 15:45:34 -0300

    Mejorar estado administrativo de bonos

--- app.py
+++ app.py
@@ -2738,19 +2738,18 @@ def estado_alumnos():
     bonos_por_vencer = 0
     bonos_vencidos = 0
     alumnos_sin_bono = 0
+    bonos_sin_creditos = 0
 
     for alumno in alumnos:
 
         dias = alumno["dias_para_vencer"]
+        creditos = alumno["creditos_disponibles"]
 
         if dias is None:
             alumnos_sin_bono += 1
 
-        elif dias <= 0:
-            bonos_vencidos += 1
-
-        elif dias <= 7:
-            bonos_por_vencer += 1
+        elif creditos == 0:
+            bonos_sin_creditos += 1
 
         elif dias <= 0:
             bonos_vencidos += 1
@@ -2769,6 +2768,7 @@ def estado_alumnos():
         bonos_vigentes=bonos_vigentes,
         bonos_por_vencer=bonos_por_vencer,
         bonos_vencidos=bonos_vencidos,
+        bonos_sin_creditos=bonos_sin_creditos,
         alumnos_sin_bono=alumnos_sin_bono
     )
 
